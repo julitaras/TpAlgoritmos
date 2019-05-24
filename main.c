f6c6c9a0684d5167fb4da1ea8f8b02ae3f053408
Julieta <julietat@lagash.com>
2019-05-24 15:11:14 -0300

    work in progress, validations

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
-/*Una empresa que se dedica a la reubicación de empleados tiene una lista de 500
-empleados (como máximo) con la siguiente información.
+/*Una empresa que se dedica a la reubicaciï¿½n de empleados tiene una lista de 500
+empleados (como mï¿½ximo) con la siguiente informaciï¿½n.
 - Nombre y apellido. Con el formato "Apellidos, Nombres".
 - Fecha de nacimiento del empleado.
 - Sexo (M, F)
 - Nacionalidad
 Las nacionalidades pueden ser: Argentina, Uruguaya, Chilena, Peruana, Boliviana,
-Paraguaya, Brasileña. Un empleado puede tener más de una nacionalidad.
+Paraguaya, Brasileï¿½a. Un empleado puede tener mï¿½s de una nacionalidad.
 Se pide armar las estructuras y el programa que permita:
 A. Realizar la carga de datos. Validar todos los datos ingresados.
 B- Mostrar un listado de personas nacidas antes de 2000 ordenado por Fecha de
 nacimiento. Mostrar todos los datos de cada empleado.
 C- Ingresar un Apellido por teclado y listar todas las mujeres que se apellidan igual y
-además nacieron en verano. Mostrar todos los datos de cada empleado.
-D- Emitir un informe de empleados uruguayos y argentinos que tengan más de una
+ademï¿½s nacieron en verano. Mostrar todos los datos de cada empleado.
+D- Emitir un informe de empleados uruguayos y argentinos que tengan mï¿½s de una
 nacionalidad. Mostrar todos los datos de cada empleado.
 E- Indicar el porcentaje de empleados argentinos*/
 #include <stdio.h>
@@ -22,12 +22,10 @@ E- Indicar el porcentaje de empleados argentinos*/
 #define Max_Empleado 500
 #define Max_Cadena_Nacionalidad 12 
 
-//Defino una matriz para las nacionalidades
-//typedef  char M_Nacionalidades[Max_Cadena_Nacionalidad][Max_Nacionalidades];
-
- typedef char T_Nacionalidad[12]; 
- typedef T_Nacionalidad V_Nacionalidad[Max_Nacionalidades];
-
+typedef char T_Nacionalidad[12]; 
+typedef T_Nacionalidad V_Nacionalidades[Max_Nacionalidades];
+//typedef V_Nacionalidades_Const = {Argentina, Uruguaya, Chilena, Peruana, Boliviana,
+//Paraguaya, BrasileÃ±a};
 //Defino un registro de fecha para hacer un manejo mejor de los datos
 typedef struct 
 {
@@ -42,49 +40,137 @@ typedef struct
 	char Apellido_Nombre[60];//Apellido, Nombre
 	T_Fecha Fecha_De_Nacimiento;
 	char Sexo;
-	V_Nacionalidad Nacionalidad;//?		
+	V_Nacionalidades Nacionalidad;//?		
 }T_Empleado;
 
+//Defino el array de Empleados 
 typedef T_Empleado VT_Empleados[Max_Empleado];
 
+//Valido la fecha de nacimiento, FALTA VALIDAR SI SE INGRESA UN CARACTER EN VEZ DE NUMERO
+int ValidarFecha(T_Fecha Fecha)
+{
+	int FechaCorrecta;
+	FechaCorrecta = 0;
+	if(Fecha.Mes >=1 && Fecha.Mes <= 12)
+	{
+		switch(Fecha.Mes)
+		{
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			if(Fecha.Dia >= 1 && Fecha.Dia <= 31)
+			{
+				FechaCorrecta = 1;
+			}; break;
+			case 4: case 6: case 9: case 11: 
+			if( Fecha.Dia >= 1 && Fecha.Dia <= 30)
+			{
+				FechaCorrecta = 1;
+			};break;
+			case 2 :  
+			if( (Fecha.Anio % 4 == 0)   &&   (Fecha.Anio % 100 != 0)   ||   (Fecha.Anio % 400 == 0) )
+			{
+				if( Fecha.Dia >= 1 && Fecha.Dia <= 29 ) 
+				{
+					FechaCorrecta = 1;
+				}
+				else 
+				{
+					if(Fecha.Dia >=1 && Fecha.Dia <= 28)
+					{
+						FechaCorrecta = 1;
+					}
+				}
+			}break;
+		}
+	}	
+	return FechaCorrecta;
+};
+/*int ValidarNombre(char Nombre_Completo_Empleado)
+{
+	//burcar en nombre algun signo especial o numero
 
-//Puede ser un solo Cargar datos que obtenga un mensaje, y el dato, ya que sino seria muy repetitiva
+};*/
+
+int Validad_Nacionalidad(T_Nacionalidad Nacionalidad)
+{
+	int EsValida;
+	EsValida = 0;
+	//Agarro el vector, lo recorro y comparo si en esa pocisicion es igual a una de las opciones 
+		if(Nacionalidad == "Argentina" || Nacionalidad== "Uruguaya" || Nacionalidad == "Chilena" || Nacionalidad == "Peruana"||Nacionalidad == "Boliviana" || Nacionalidad== "Brasileï¿½a" )
+		{
+			EsValida = 1;
+		}
+	
+	return EsValida;
+};
+
+void Cargar_Nacionalidades(V_Nacionalidades Nacionalidades, int *ml_Nacionalidad)
+{
+	int i, Continuar, EsCorrecta;
+	i = 0;
+	Continuar = 0;
+	do
+	{
+		printf("Ingrese la/s nacionalidades:\n");
+		fgets(Nacionalidades[i], 12, stdin);
+		EsCorrecta = Validad_Nacionalidad(Nacionalidades[i]);
+		if(EsCorrecta == 0)
+		{
+			printf("No existe esa nacionalidad\n");
+		}
+		else{
+		printf("Desea continuar cargando nacionalidades? 0 PARA CONTINUAR,\n 1 PARA FINALIZAR: \n");//Mejorar con el SI y el NO
+		scanf("%i", &Continuar);
+		fflush(stdin);
+		}
+		i++;
+	}
+	while((*ml_Nacionalidad > 5) || Continuar != 1 || EsCorrecta != 1);
+	
+}
+//Cargo los datos del empleado, puedo modulareizarlo mas haciendo diferentes funciones que cargue diferentes daos pq se hace muy largo 
 void Cargar_Empleado(VT_Empleados Empleados, int *ml)
 {
-	int Continuar, i/*, ml_Nacionalidad*/;
+	int Continuar, i, EsCorrecta, ml_Nacionalidad;
 	Continuar = 0;
 	i = 0;
+
 	printf("Ingrese los datos de los empleados:\n");
 	do 
 	{
+		//Cargar_Nombre_Completo_Empleado();
 		printf("Ingrese el Apellido y Nombre:\n");
 		fgets(Empleados[i].Apellido_Nombre, 60, stdin);
-		
-		printf("Ingrese la fecha de nacimiento:\n");
-		printf("Dia: ");
-		scanf("%i",&Empleados[i].Fecha_De_Nacimiento.Dia);
-		fflush(stdin);
-		printf("Mes: ");
-		scanf("%i",&Empleados[i].Fecha_De_Nacimiento.Mes);
-		fflush(stdin);
-		printf("Año: ");
-		scanf("%i",&Empleados[i].Fecha_De_Nacimiento.Anio);
-		fflush(stdin);
-		
+		//ValidarNombre(Empleados[i].Apellido_Nombre); no sea numeros
+		//Cargar_Fecha_Nacimiento();
+		do
+		{
+			printf("Ingrese la fecha de nacimiento:\n");
+			printf("Dia: ");
+			scanf("%i",&Empleados[i].Fecha_De_Nacimiento.Dia);
+			fflush(stdin);
+			printf("Mes: ");
+			scanf("%i",&Empleados[i].Fecha_De_Nacimiento.Mes);
+			fflush(stdin);
+			printf("AÃ±o: ");
+			scanf("%i",&Empleados[i].Fecha_De_Nacimiento.Anio);
+			fflush(stdin);
+			EsCorrecta = ValidarFecha(Empleados[i].Fecha_De_Nacimiento);
+			if(EsCorrecta == 0)
+			{
+			 printf("Ingreso mal la fecha %i/%i/%i\n",Empleados[i].Fecha_De_Nacimiento.Dia, Empleados[i].Fecha_De_Nacimiento.Mes, Empleados[i].Fecha_De_Nacimiento.Anio);
+			}
+		}
+		while(EsCorrecta != 1);
+		//Cargar_Genero();
+		//do{
 		printf("Ingrese el Sexo:\n");
 		scanf(" %c",&Empleados[i].Sexo);
 		fflush(stdin);
+		//EsCorrecta = ValidarSexo(Empleados[i].Sexo);
+		//}while(EsCorrecta != 0);
+		Cargar_Nacionalidades(Empleados[i].Nacionalidad, &ml_Nacionalidad);
 		
-		//Cargar_Nacionalidades(Empleados[i].Nacionalidad, &ml_Nacionalidad);
-		do{
-		
-		printf("Ingrese la/s nacionalidades:\n");
-		fgets(Empleados[i].Nacionalidad[i], 12, stdin);
-		printf("Desea continuar cargando nacionalidades? 0 PARA CONTINUAR,\n 1 PARA FINALIZAR: \n");
-		scanf("%i", &Continuar);
-		fflush(stdin);
-		}while(Continuar != 1);
-		printf("Desea continuar cargando empleados? 0 PARA CONTINUAR,\n 1 PARA FINALIZAR: \n");
+		printf("Desea continuar cargando empleados? 0 PARA CONTINUAR,\n 1 PARA FINALIZAR: \n"); //Mejorar con el SI y el NO
 		scanf("%i", &Continuar);
 		fflush(stdin);
 		i++;
@@ -97,5 +183,5 @@ void main()
 	int ml;
 	 
 	Cargar_Empleado(Empleados, &ml);
-	
+	//Validar_Empleado();
 }
